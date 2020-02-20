7969028cf6c027ad44bafe798962e9d1efa6a24a
sandy <sandytang2406@gmail.com>
2020-02-20 19:16:32 +0100

    classes and membervariables implemented

---
+++ src/main/java/Question.java
@@ -0,0 +1,16 @@
+public class Question {
+
+    private String answer;
+
+    public Question(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,26 @@
 public class Student {
+
+    private int id;
+    private String schoolClasses;
+
+    public Student(int id, String schoolClasses) {
+        this.id = id;
+        this.schoolClasses = schoolClasses;
+    }
+
+    public String getSchoolClasses() {
+        return schoolClasses;
+    }
+
+    public void setSchoolClasses(String schoolClasses) {
+        this.schoolClasses = schoolClasses;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }

607154a65530b6fe245ebb5c1a14a664887c14dc
sandy <sandytang2406@gmail.com>
2020-02-22 15:08:09 +0100

    javaFx-project created

---
+++ JavafxProjekt/src/main/java/at/htl/demo/Teacher.java
@@ -0,0 +1,23 @@
+package at.htl.demo;
+
+public class Teacher {
+
+    private String name;
+    private String subject;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}

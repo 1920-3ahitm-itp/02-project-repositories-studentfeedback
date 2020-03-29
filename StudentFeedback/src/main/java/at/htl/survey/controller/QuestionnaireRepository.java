a978794856077b65fc3bd405c4a8c60333609334
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-03-29 21:34:58 +0200

    add Repository test

---
+++ StudentFeedback/src/main/java/at/htl/survey/controller/QuestionnaireRepository.java
@@ -0,0 +1,26 @@
+package at.htl.survey.controller;
+
+import java.util.List;
+
+public class QuestionnaireRepository implements Persistent{
+
+  @Override
+  public void save(Object entity) {
+
+  }
+
+  @Override
+  public void delete(long id) {
+
+  }
+
+  @Override
+  public List findAll() {
+    return null;
+  }
+
+  @Override
+  public Object findById(long id) {
+    return null;
+  }
+}
---
+++ StudentFeedback/src/test/java/at/htl/survey/controller/QuestionnaireRepositoryTest.java
@@ -0,0 +1,24 @@
+package at.htl.survey.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionnaireRepositoryTest {
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void findAll() {
+  }
+
+  @Test
+  void findById() {
+  }
+}

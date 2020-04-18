be561a0b3b2d105b6cfe4b6b73f743a43972eb70
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2020-04-19 00:08:41 +0200

    QuestionnaireRepository save() method added

--- StudentFeedback/src/test/java/at/htl/survey/controller/QuestionnaireRepositoryTest.java
+++ StudentFeedback/src/test/java/at/htl/survey/controller/QuestionnaireRepositoryTest.java
@@ -1,13 +1,30 @@
 package at.htl.survey.controller;
 
+import at.htl.survey.model.Questionnaire;
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.db.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionnaireRepositoryTest {
 
+  static QuestionnaireRepository repo;
+
   @Test
   void save() {
+    Questionnaire questionnaire = new Questionnaire(1, "Männlich");
+
+    questionnaire.setQn_id(12);
+
+    repo.save(questionnaire);
+    Table table = new Table(Database.getDataSource(), "Questionnaire");
+
+    assertThat(table).row(table.getRowsList().size() - 1)
+            .value("Männlich").isEqualTo("watering pots, cookware, ...");
+
   }
 
   @Test

78e8a13d0ba618b771522259202ed67d851d4eab
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2020-04-18 23:49:57 +0200

    QuestionnaireRepository save() method added

--- StudentFeedback/src/main/java/at/htl/survey/controller/Persistent.java
+++ StudentFeedback/src/main/java/at/htl/survey/controller/Persistent.java
@@ -1,8 +1,11 @@
 package at.htl.survey.controller;
 
+import at.htl.survey.model.Questionnaire;
+
 import java.util.List;
 
 public interface Persistent<T> {
+
     public void save(T entity);
     public void delete(long id);
     public List<T> findAll();
--- StudentFeedback/src/main/java/at/htl/survey/controller/QuestionnaireRepository.java
+++ StudentFeedback/src/main/java/at/htl/survey/controller/QuestionnaireRepository.java
@@ -1,11 +1,37 @@
 package at.htl.survey.controller;
 
+import at.htl.survey.model.Questionnaire;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
-public class QuestionnaireRepository implements Persistent{
+import static at.htl.survey.controller.Database.*;
+
+public class QuestionnaireRepository implements Persistent<Questionnaire>{
+
+  private DataSource dataSource = Database.getDataSource();
 
   @Override
-  public void save(Object entity) {
+  public void save(Questionnaire questionnaire) {
+
+
+    try (Connection conn = DriverManager.getConnection(URL,USERNAME, PASSWORD)) {
+
+      String sql = "INSERT INTO questionnaire (qn_description) " +
+              "VALUES (?)";
+      try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+        pstmt.setString(1,questionnaire.getQn_description());
+        pstmt.executeUpdate();
+
+      }
+    } catch (SQLException e) {
+      System.err.println(e.getMessage());
+    }
 
   }
 
@@ -20,7 +46,7 @@ public class QuestionnaireRepository implements Persistent{
   }
 
   @Override
-  public Object findById(long id) {
+  public Questionnaire findById(long id) {
     return null;
   }
 }

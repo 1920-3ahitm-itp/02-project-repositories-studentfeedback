9a90cc32625985e17eba479ca482462699a6b412
sandy <sandytang2406@gmail.com>
2020-04-19 20:20:14 +0200

    Gantt Diagram updated

--- sql/feedb_insert.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO teacher (teacher_vname, teacher_nname, teacher_subject) VALUES ('Thomas', 'Stütz', 'Softwareentwicklung');
-INSERT INTO teacher (teacher_vname, teacher_nname, teacher_subject) VALUES ('Robert', 'Reder', 'Medientechnik');
-INSERT INTO teacher (teacher_vname, teacher_nname, teacher_subject) VALUES ('Michael', 'Holzmann', 'Mathematik');
-
-INSERT INTO survey (teacher_id, survey_date, class_name, subject) VALUES (12, '2020-04-01 08:00:00', '3AHITM', 'Softwareentwicklung');
-INSERT INTO survey (teacher_id, survey_date, class_name, subject) VALUES (14, '2020-05-02 10:00:00', '4AHITM', 'Systemtechnik');
-INSERT INTO survey (teacher_id, survey_date, class_name, subject) VALUES (10, '2020-06-05 12:00:00', '1AHITM', 'Mathematik');
